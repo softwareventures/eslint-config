1bf9be6d4491e6a05cd20623fd9fade06aaa8aa6
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-05-10 05:09:38 +0100

    fix: only apply TypeScript rules to TypeScript files

--- index.ts
+++ index.ts
@@ -4,19 +4,22 @@ const config: Linter.Config = {
     env: {
         node: true
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint"
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        project: "tsconfig.json",
-        sourceType: "module"
-    },
-    plugins: [
-        "@typescript-eslint"
+    extends: ["eslint:recommended", "prettier"],
+    plugins: ["@typescript-eslint"],
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"],
+            extends: [
+                "plugin:@typescript-eslint/eslint-recommended",
+                "plugin:@typescript-eslint/recommended",
+                "prettier/@typescript-eslint"
+            ],
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                project: "tsconfig.json",
+                sourceType: "module"
+            }
+        }
     ]
 };
 
