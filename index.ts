e579006a4ed4c387f0532cbfef8247524708a189
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-05-10 11:46:07 +0100

    feat: add rules to bring config in line with Software Ventures tslint-rules

--- index.ts
+++ index.ts
@@ -18,6 +18,137 @@ const config: Linter.Config = {
             parserOptions: {
                 project: "tsconfig.json",
                 sourceType: "module"
+            },
+            rules: {
+                "@typescript-eslint/array-type": [
+                    "error",
+                    {
+                        default: "array-simple"
+                    }
+                ],
+                "@typescript-eslint/await-thenable": "error",
+                "@typescript-eslint/ban-types": [
+                    "error",
+                    {
+                        types: {
+                            Object: {
+                                message: "Avoid using the `Object` type. Did you mean `object`?"
+                            },
+                            Function: {
+                                message:
+                                    "Avoid using the `Function` type. Prefer a specific function type, like `() => void`."
+                            },
+                            Boolean: {
+                                message: "Avoid using the `Boolean` type. Did you mean `boolean`?"
+                            },
+                            Number: {
+                                message: "Avoid using the `Number` type. Did you mean `number`?"
+                            },
+                            String: {
+                                message: "Avoid using the `String` type. Did you mean `string`?"
+                            },
+                            Symbol: {
+                                message: "Avoid using the `Symbol` type. Did you mean `symbol`?"
+                            }
+                        }
+                    }
+                ],
+                "@typescript-eslint/explicit-function-return-type": "error",
+                "@typescript-eslint/explicit-member-accessibility": [
+                    "error",
+                    {
+                        accessibility: "explicit"
+                    }
+                ],
+                "@typescript-eslint/interface-name-prefix": "off",
+                "@typescript-eslint/member-delimiter-style": [
+                    "error",
+                    {
+                        multiline: {
+                            delimiter: "semi",
+                            requireLast: true
+                        },
+                        singleline: {
+                            delimiter: "semi",
+                            requireLast: false
+                        }
+                    }
+                ],
+                "@typescript-eslint/member-ordering": "error",
+                "@typescript-eslint/no-explicit-any": "off",
+                "@typescript-eslint/no-extraneous-class": "error",
+                "@typescript-eslint/no-floating-promises": "error",
+                "@typescript-eslint/no-for-in-array": "error",
+                "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+                "@typescript-eslint/no-unnecessary-type-assertion": "error",
+                "@typescript-eslint/no-use-before-define": "off",
+                "@typescript-eslint/prefer-for-of": "off",
+                "@typescript-eslint/prefer-function-type": "error",
+                "@typescript-eslint/promise-function-async": "error",
+                "@typescript-eslint/triple-slash-reference": [
+                    "error",
+                    {
+                        path: "always",
+                        types: "prefer-import",
+                        lib: "always"
+                    }
+                ],
+                "@typescript-eslint/unbound-method": "error",
+                "@typescript-eslint/unified-signatures": "error",
+                "arrow-body-style": "error",
+                "arrow-parens": ["off", "always"],
+                "brace-style": ["error", "1tbs"],
+                "class-methods-use-this": "error",
+                "comma-dangle": "error",
+                "complexity": "off",
+                "curly": "error",
+                "dot-notation": "error",
+                "eqeqeq": ["error", "smart"],
+                "guard-for-in": "error",
+                "id-blacklist": "error",
+                "id-match": "error",
+                "import/no-extraneous-dependencies": "error",
+                "import/order": "error",
+                "jsdoc/check-alignment": "error",
+                "jsdoc/check-indentation": "error",
+                "jsdoc/newline-after-description": "error",
+                "jsdoc/no-types": "error",
+                "max-classes-per-file": "off",
+                "new-parens": "error",
+                "no-caller": "error",
+                "no-duplicate-imports": "error",
+                "no-empty": "off",
+                "no-eval": "error",
+                "no-extra-bind": "error",
+                "no-irregular-whitespace": "error",
+                "no-new-func": "error",
+                "no-new-wrappers": "error",
+                "no-proto": "error",
+                "no-return-await": "error",
+                "no-script-url": "error",
+                "no-self-compare": "error",
+                "no-throw-literal": "error",
+                "no-undef-init": "error",
+                "no-underscore-dangle": "error",
+                "no-unused-expressions": "error",
+                "no-useless-constructor": "error",
+                "no-void": "error",
+                "object-shorthand": "error",
+                "one-var": ["error", "never"],
+                "prefer-arrow/prefer-arrow-functions": "error",
+                "prefer-object-spread": "error",
+                "radix": "error",
+                "@typescript-eslint/tslint/config": [
+                    "error",
+                    {
+                        rules: {
+                            "no-inferred-empty-object-type": true,
+                            "no-mergeable-namespace": true,
+                            "no-unnecessary-callback-wrapper": true,
+                            "prefer-while": true
+                        }
+                    }
+                ]
             }
         }
     ]
