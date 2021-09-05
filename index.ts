4e722b5009e7e0a29f3b4b6e72b126440f1f267f
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-09-05 22:45:41 +0100

    feat(no-loop-func): disallow function declarations with unsafe references inside loop statements
    BREAKING CHANGE: Functions declared inside loop statements may not reference variables from the loop
    scope declared with `var`, or variables declared with `let` or `var` from any parent scope where the
    variable is modified inside or outside the loop. Such code is unlikely to behave as the author
    intended and is usually indicative of an error.

--- index.ts
+++ index.ts
@@ -244,7 +244,8 @@ const config: Linter.Config = {
                 leadingUnderscore: "forbid",
                 trailingUnderscore: "forbid"
             }
-        ]
+        ],
+        "@typescript-eslint/no-loop-func": "error",
     }
 };
 
