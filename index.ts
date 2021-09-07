a236986d2c587bd8ae52f361eff6ddd2a9244d39
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-09-07 05:34:17 +0100

    feat(non-existent-operator): disallow confusing assignment operations
    BREAKING CHANGE: Use of the assignment operator `=` paired with `+`, `-` or `!` is now disallowed in
    contexts where the pairing could be confused for `+=`, `-=`, or `!=`.

--- index.ts
+++ index.ts
@@ -264,6 +264,7 @@ const config: Linter.Config = {
         "sonarjs/no-identical-expressions": "error",
         "sonarjs/no-one-iteration-loop": "error",
         "sonarjs/no-use-of-empty-return-value": "error",
+        "sonarjs/non-existent-operator": "error",
         "sonarjs/prefer-while": "error",
         "@typescript-eslint/naming-convention": [
             "error",
