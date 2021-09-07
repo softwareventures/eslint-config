c6f1943166d356216edc5eb1d20284cc0848f8e2
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-09-07 06:23:22 +0100

    feat(no-redundant-jump): disallow redundant jump statements
    BREAKING CHANGE: Redundant `return`, `break`, or `continue` statements are now considered an error
    in most cases. See
    [no-redundant-jump](https://github.com/SonarSource/eslint-plugin-sonarjs/blob/master/docs/rules/no-redundant-jump.md).

--- index.ts
+++ index.ts
@@ -270,6 +270,7 @@ const config: Linter.Config = {
         "sonarjs/no-inverted-boolean-check": "error",
         "sonarjs/no-one-iteration-loop": "error",
         "sonarjs/no-redundant-boolean": "error",
+        "sonarjs/no-redundant-jump": "error",
         "sonarjs/no-use-of-empty-return-value": "error",
         "sonarjs/non-existent-operator": "error",
         "sonarjs/prefer-while": "error",
