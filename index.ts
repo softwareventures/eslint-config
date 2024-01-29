d2a39fa6b8080fd6d27b53133516d2d58f5923b0
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-01-29 00:46:55 +0000

    fix(ban-types): replace fixWith rule for `Function` with a suggestion instead
    The suggestion is not always valid, so it is
    better as a suggestion than as a fix.

--- index.ts
+++ index.ts
@@ -63,7 +63,7 @@ const config: Linter.Config = {
                                     "It also accepts things like class declarations, which may throw at runtime as they may not be called with `new`.",
                                     "If you are expecting a function to accept certain arguments, you should explicitly define the function shape."
                                 ].join("\n"),
-                                fixWith: "() => void"
+                                suggest: ["() => void"]
                             }
                         },
                         extendDefaults: true
