c8555da40d173ecaa672561dfee66602e33df515
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-09-10 22:31:07 +0100

    fix(deps): delete obsolete ban-types rule
    This rule no longer exists in typescript-eslint
    v8.

--- index.ts
+++ index.ts
@@ -39,7 +39,6 @@ const config: Linter.Config = {
                 ],
                 "@typescript-eslint/ban-tslint-comment": "error",
                 "@typescript-eslint/ban-ts-comment": "error",
-                "@typescript-eslint/ban-types": "off",
                 "@typescript-eslint/class-literal-property-style": "error",
                 "@typescript-eslint/consistent-type-assertions": [
                     "error",
